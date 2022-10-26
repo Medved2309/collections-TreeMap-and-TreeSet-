ac07f4af04680631b6cfbad838be28971f864386
Andrey Dmitrievich <medved2309@gmail.com>
2022-10-26 12:27:34 +0500

    reshenie

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,24 @@
+import person.Person;
+import personComparator.PersonComparator;
+
+import java.util.Collections;
+import java.util.TreeSet;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+
+        PersonComparator personComparator = new PersonComparator(2);
+        TreeSet<Person> people = new TreeSet<>(personComparator);
+        people.add(new Person("Игорь", "Корсаков", 28));
+        people.add(new Person("Олег", "Корсаков", 48));
+        people.add(new Person("Ольга", "Иванова", 15));
+        people.add(new Person("Махмуд", "Тургун Шарифзаде", 18));
+        people.add(new Person("Махмуд", "Тургун Шариф", 10));
+        people.add(new Person("Мах", "Тур Шаиф", 10));
+        people.add(new Person("Маха", "Тур Шаиф Али", 19));
+        for (Person o : people) {
+            System.out.printf("Фамилия: %s. Имя: %s. Возраст: %d\n", o.getSurname(), o.getName(), o.getAge());
+        }
     }
 }
---
+++ src/personComparator/PersonComparator.java
@@ -0,0 +1,42 @@
+package personComparator;
+
+import person.Person;
+
+import java.util.Comparator;
+
+public class PersonComparator implements Comparator<Person> {
+
+    private int wordNumb;
+
+
+    public PersonComparator(int wordNumb) {
+        this.wordNumb = wordNumb;
+    }
+
+//        @Override
+//    public int compare(Person o1, Person o2) {
+//        int lengthSurnameFistPerson;
+//        lengthSurnameFistPerson = o1.getSurname().split("\\S").length;
+//        int lengthSurnameSecondPerson = o2.getSurname().split("\\S").length;
+//        if (lengthSurnameFistPerson == lengthSurnameSecondPerson) {
+//            return Integer.compare(o1.getAge(), o2.getAge());
+//        }
+//
+//        return lengthSurnameFistPerson > lengthSurnameSecondPerson ? 1 : -1;
+//    }
+
+    @Override
+    public int compare(Person o1, Person o2) {
+        int lengthSurnameFistPerson = o1.getSurname().split("\\S").length;
+        int lengthSurnameSecondPerson = o2.getSurname().split("\\S").length;
+        if (lengthSurnameFistPerson > wordNumb && lengthSurnameSecondPerson > wordNumb) {
+            return Integer.compare(o1.getAge(), o2.getAge());
+        }
+        if (lengthSurnameFistPerson == lengthSurnameSecondPerson) {
+            return Integer.compare(o1.getAge(), o2.getAge());
+        }
+        return lengthSurnameFistPerson > lengthSurnameSecondPerson ? 1 : -1;
+    }
+
+
+}
