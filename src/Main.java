d55e50bce120637371ae4d1111661a0b0b396652
Andrey Dmitrievich Grishilo <medved2309@yandex.ru>
2022-10-26 23:01:50 +0500

    reshenie correction lamda

--- src/Main.java
+++ src/Main.java
@@ -1,11 +1,20 @@
 import person.Person;
+
 import java.util.Comparator;
 import java.util.TreeSet;
 
 public class Main {
     public static void main(String[] args) {
 
-        Comparator<Person> comparator = Main::compare;
+        Comparator<Person> comparator = (o1, o2) -> {
+            int lengthSurnameFistPerson = o1.getSurname().split("\\S").length;
+            int lengthSurnameSecondPerson = o2.getSurname().split("\\S").length;
+            if (lengthSurnameFistPerson == lengthSurnameSecondPerson) {
+                return Integer.compare(o1.getAge(), o2.getAge());
+            }
+            return lengthSurnameFistPerson > lengthSurnameSecondPerson ? 1 : -1;
+        };
+        
         TreeSet<Person> people = new TreeSet<>(comparator);
         people.add(new Person("Игорь", "Корсаков", 28));
         people.add(new Person("Олег", "Корсаков", 48));
@@ -21,14 +30,14 @@ public class Main {
 
     }
 
-    private static int compare(Person o1, Person o2) {
-
-        int lengthSurnameFistPerson = o1.getSurname().split("\\S").length;
-        int lengthSurnameSecondPerson = o2.getSurname().split("\\S").length;
-        if (lengthSurnameFistPerson == lengthSurnameSecondPerson) {
-            return Integer.compare(o1.getAge(), o2.getAge());
-        }
-
-        return lengthSurnameFistPerson > lengthSurnameSecondPerson ? 1 : -1;
-    }
+//    private static int compare(Person o1, Person o2) {
+//
+//        int lengthSurnameFistPerson = o1.getSurname().split("\\S").length;
+//        int lengthSurnameSecondPerson = o2.getSurname().split("\\S").length;
+//        if (lengthSurnameFistPerson == lengthSurnameSecondPerson) {
+//            return Integer.compare(o1.getAge(), o2.getAge());
+//        }
+//
+//        return lengthSurnameFistPerson > lengthSurnameSecondPerson ? 1 : -1;
+//    }
 }
